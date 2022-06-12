a1b2c23d702b1d2703ac749243595e241f3c76f9
Your Name <you@example.com>
2022-06-12 21:07:09 +0300

    aa

--- NetScanner/netscan.py
+++ NetScanner/netscan.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
+from numpy import broadcast
 import scapy.all as scapy 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_requst = scapy.ARP()
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    print(broadcast.summary())
+
+scan("10.0.2.1/24")
